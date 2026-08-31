73c7941a8a634f6361144e2a1b81b8ae7fef2255
andreananni050-droid <andrea.nanni.050@gmail.com>
2026-08-31 13:36:40 +0200

    descrizione modulo Query minmax

--- lista_di_adiacenza.cpp
+++ lista_di_adiacenza.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<vector<array<long long,2>>> v;
+vector<vector<array<long long,2>>> adj(ccount+1);
 void build_lista_di_adiacenza(){
     for(auto u:M){ // u è un pair<const array<ll,2>,ll>
         int x = u.first[0];
         int y = u.first[1];
-        v[x].push_back({y,u.second});
+        adj[x].push_back({y,u.second});
     }
 }
